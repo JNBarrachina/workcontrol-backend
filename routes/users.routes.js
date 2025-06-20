dd099e251eef3402fa0b99f3195dc383bdeac173
Tomas-crypto14 <tomasignaciocapalbo@gmail.com>
2025-06-20 23:44:17 +0200

    Añadido codigo del subproject

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 
 const {loginUser} = require("../controllers/users.controller")
+const {createSubproject} = require("../controllers/subproject.controller")
 const router = express.Router();
 
 router.post("/login", loginUser);
-
+router.post("/subproject", createSubproject)
 module.exports = router;
