b311b52e6c7125bf2c7ff2122c58dc13d3dd6a8b
Joan <joan1998bocairent@gmail.com>
2025-06-22 13:07:38 +0200

    Crete two endpoints for get data

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const express = require("express");
 const cors = require("cors");
 const db = require("./db");
 
-require("./models/Employee");
-require("./models/Project");
-require("./models/Subproject");
+const Employee =  require("./models/Employee");
+const Prject =  require("./models/Project");
+const Subproject =  require("./models/Subproject");
 require("./models/DayCode");
 require("./models/EmployeeDailyCalendar");
 require("./models/EmployeeWorkEntry");
@@ -24,6 +24,51 @@ const main = () => {
     app.use(express.json());
 
     app.use("/users", usersRouter);
+
+    //ADMIN
+    app.get('/employeeds_projects', async (req, res)=>{
+        try{
+            const results = await db.sequelize.query(`
+                select e.name as Employeed, p.name as 'Assigned Project', ea.assignedAT as 'Date Assigned'
+                from workcontroldb.employees e, workcontroldb.projects p, workcontroldb.employeeprojectassignments ea 
+                where ea.EmployeeId = e.id and ea.ProjectId = p.id
+                order by e.name ASC;
+            `);
+
+            if (!results) {
+                res.status(400).send("EMPLOYEEDS_DON'T_HAVE_ASSIGNED_PROJECTS");
+                return;
+            }
+
+            res.status(201).send( results );
+
+        } catch (err){
+            res.status(500).send(err);
+        }
+    });
+
+    //USER
+    app.post('/employeed_assigned', async (req, res)=>{
+        try{
+            const {rol, name} = req?.body;
+
+            const results = await db.sequelize.query(`
+                select e.name as 'Employeed', p.name as 'Assigned Project', ea.assignedAT as 'Date Assigned'
+                from workcontroldb.employees e, workcontroldb.projects p, workcontroldb.employeeprojectassignments ea 
+                where ea.EmployeeId = e.id and ea.ProjectId = p.id and e.name = '${name}';
+            `);
+
+            if (!results) {
+                res.status(400).send("THE_EMPLOYEED_DON'T_HAVE_ASSIGNED_PROJECTS");
+                return;
+            }
+
+            res.status(201).send( results );
+
+        } catch (err){
+            res.status(500).send(err);
+        }
+    });
 
     db.sequelize
         .sync({alter: true})
