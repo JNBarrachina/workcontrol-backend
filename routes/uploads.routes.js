1d42ce72f495ddd48c90b4e992271fdfe4b64330
Joan <joan1998bocairent@gmail.com>
2025-07-13 14:10:20 +0200

    Añadida carpeta uploads que recibira los archivos del fromtend, añadida nueva ruta par que los archivos funcionen en el endpoint

---
+++ routes/uploads.routes.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+
+// subir archivos al sevidor
+router.post("/", async (req, res) => {
+        const archivo = req.files?.firma;
+        const user = req.body?.user;
+
+        if (!archivo) {
+            return res.status(400).json({ error: "No se envió ningún archivo." });
+        }
+
+        const uploadsDir = path.join(__dirname, "../uploads");
+        if (!fs.existsSync(uploadsDir)) {
+            fs.mkdirSync(uploadsDir);
+        }
+
+        const nombreArchivo = `firma_${user}_${Date.now()}.png`;
+        const rutaArchivo = path.join(uploadsDir, nombreArchivo);
+
+        archivo.mv(rutaArchivo, (err) => {
+            if (err) {
+            console.error("Error al guardar el archivo:", err);
+            return res.status(500).json({ error: "No se pudo guardar el archivo." });
+            }
+
+            res.json({
+            mensaje: "Firma guardada correctamente.",
+            ruta: `/uploads/${nombreArchivo}`,
+            nombre: nombreArchivo,
+            });
+        });
+    });
+
+
+module.exports = router;
