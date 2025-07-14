7a556daf11517552c069941f5c03e203e807131d
Joan <joan1998bocairent@gmail.com>
2025-07-14 21:26:23 +0200

    error contenido del backend

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const { loginUser } = require("../controllers/users.controller");
 const { registerUser } = require("../controllers/users.controller");
+/* Este impor rompe el controlador aun no existe
 const { createSubproject } = require("../controllers/subproject.controller");
+*/
+
+'../controllers/users.controller.js'
+
+
 const {
     createWorkEntry,
 } = require("../controllers/employeeworkentry.controller");
@@ -23,7 +29,10 @@ router.get("/userprojects/:id", getUserProjects);
 router.get("/employee", getEmployeeId);
 router.post("/newworkentry", createWorkEntry);
 router.delete("/rmworkentry", deleteEmployeeWorkEntry);
+
+/* Lo mismo el controlador aun no existe y rompe
 router.post("/subproject", createSubproject);
+*/
 
 
 module.exports = router;
