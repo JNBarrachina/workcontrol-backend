8bc708af3bc04803ac8ad59c3c8a4c0b3ee2d2de
Tomas-crypto14 <tomasignaciocapalbo@gmail.com>
2025-07-14 13:28:03 +0200

    Actualizado con Register

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,18 +1,28 @@
 const express = require("express");
 
-const {loginUser} = require("../controllers/users.controller")
-const {createSubproject} = require("../controllers/subproject.controller")
-const {createWorkEntry} = require("../controllers/employeeworkentry.controller")
-const {getEmployeeId} = require("../controllers/employeeworkentry.controller")
-const {deleteEmployeeWorkEntry} = require("../controllers/employeeworkentry.controller");
-const {getUserProjects} = require("../controllers/employeeworkentry.controller");
+const { loginUser } = require("../controllers/users.controller");
+const { registerUser } = require("../controllers/users.controller");
+const { createSubproject } = require("../controllers/subproject.controller");
+const {
+    createWorkEntry,
+} = require("../controllers/employeeworkentry.controller");
+const {
+    getEmployeeId,
+} = require("../controllers/employeeworkentry.controller");
+const {
+    deleteEmployeeWorkEntry,
+} = require("../controllers/employeeworkentry.controller");
+const {
+    getUserProjects,
+} = require("../controllers/employeeworkentry.controller");
 const router = express.Router();
 
+router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.get("/userprojects/:id", getUserProjects)
-router.get("/employee", getEmployeeId)
-router.post("/newworkentry", createWorkEntry)
-router.delete("/rmworkentry", deleteEmployeeWorkEntry)
+router.get("/userprojects/:id", getUserProjects);
+router.get("/employee", getEmployeeId);
+router.post("/newworkentry", createWorkEntry);
+router.delete("/rmworkentry", deleteEmployeeWorkEntry);
 
-router.post("/subproject", createSubproject)
+router.post("/subproject", createSubproject);
 module.exports = router;
