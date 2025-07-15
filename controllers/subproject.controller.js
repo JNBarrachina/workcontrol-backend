48ce14405f90a2ba0ef44dcb3d9ebe6ca7ec5669
Tomas-crypto14 <tomasignaciocapalbo@gmail.com>
2025-07-15 09:48:03 +0200

    Actualizado

---
+++ controllers/subproject.controller.js
@@ -0,0 +1,13 @@
+const SubProject = require("../models/Subproject");
+
+const createSubproject = async (req, res) => {
+    const subprojectname = req.body.name;
+
+    const createdSubproject = await SubProject.create({
+        name: subprojectname,
+    });
+
+    res.status(201).send({ id: createdSubproject.id });
+};
+
+exports.createSubproject = createSubproject;
